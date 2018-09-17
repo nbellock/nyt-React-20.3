0dd5c7a04022e5b042aff573f85042ebc6dae7b1
nikki bellock <nbellock@gmail.com>
2018-09-16 21:57:18 -0600

    'message'

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// @author: Thomas Thompson
-// @github: tomtom28
-// @comment: HW for Week 19 - NY Time Article Search with ReactJS! Whoo!
-
-
 // Require Node Modules
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -27,22 +22,21 @@ app.use(express.static(process.cwd() + '/public'));
 // Database Configuration with Mongoose
 // ---------------------------------------------------------------------------------------------------------------
 // Connect to localhost if not a production environment
-if(process.env.NODE_ENV == 'production'){
+if (process.env.NODE_ENV == 'production') {
   // Gotten using `heroku config | grep MONGODB_URI` command in Command Line
   mongoose.connect('mongodb://heroku_kbdv0v69:860jh71jd1iu5m5639gjr0gg9l@ds129028.mlab.com:29028/heroku_kbdv0v69');
-}
-else{
+} else {
   mongoose.connect('mongodb://localhost/nytreact');
 }
 var db = mongoose.connection;
 
 // Show any Mongoose errors
-db.on('error', function(err) {
+db.on('error', function (err) {
   console.log('Mongoose Error: ', err);
 });
 
 // Once logged in to the db through mongoose, log a success message
-db.once('open', function() {
+db.once('open', function () {
   console.log('Mongoose connection successful.');
 });
 
@@ -60,6 +54,6 @@ app.use('/', router);
 
 // Launch App
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+app.listen(port, function () {
   console.log('Running on port: ' + port);
 });
